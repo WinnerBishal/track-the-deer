695e1be594d79894b65a70225b55fe18653ffacb
WinnerBishal <winnerbishal9848@gmail.com>
2025-07-22 16:52:03 -0500

    Added function to process evaluate models in bulk

--- notebooks/evaluate.py
+++ notebooks/evaluate.py
@@ -6,35 +6,139 @@ from pathlib import Path
 import pandas as pd
 from datetime import datetime
 import ultralytics as ultics
+import cv2
+import matplotlib.pyplot as plt
 
 
-best_model_dir = "../experiments/Lila.v1.3k_y10s_ep100/weights/best.pt"
+yolo10n = "../experiments/Lila.v1.3k_y10n_ep100/weights/best.pt"
+yolo11n = "../experiments/Lila.v1.3k_y11_ep100/weights/best.pt"
+yolo8n = "../experiments/Lila.v1.3k_y8_ep100/weights/best.pt"
 
-model = ultics.YOLO(best_model_dir)
-data_config = "../data/Lila.v1.3k/data.yaml"
+yolo9s = "../experiments/Lila.v1.3k_y9s_ep100/weights/best.pt"
+yolo10s = "../experiments/Lila.v1.3k_y10s_ep100/weights/best.pt"
+yolo11s = "../experiments/Lila.v1.3k_y11s_ep100/weights/best.pt"
 
-results = model.val(data=data_config,
-                    split='val',
-                    save_json=True,  
-                    save_txt=True,   
-                    save_conf=True,  
-                    save_hybrid=True,  
-                    plots=True)  
+yolo11m = "../experiments/Lila.v1.3k_y11m_ep100/weights/best.pt"
+yolo11x = "../experiments/Lila.v1.3k_y11x_ep100/weights/best.pt"
 
-metrics = {
-            'model_path': best_model_dir,
-            'experiment_name': Path(best_model_dir).parent.parent.name,
-            'precision': results.box.mp if hasattr(results.box, 'mp') else 0.0,
-            'recall': results.box.mr if hasattr(results.box, 'mr') else 0.0,
-            'map50': results.box.map50 if hasattr(results.box, 'map50') else 0.0,
-            'map50_95': results.box.map if hasattr(results.box, 'map') else 0.0,
-            'fitness': results.fitness if hasattr(results, 'fitness') else 0.0,
+all_models = [yolo10n, yolo11n, yolo8n, yolo9s, yolo10s, yolo11s, yolo11m, yolo11x]
+
+def evaluate(all_models, data_config = "../data/Lila.v1.3k/data.yaml"):
+    results = []
+    for model_path in all_models:
+        model = ultics.YOLO(model_path)
+        result = model.val(data=data_config,
+                           split='val',
+                           save_json=True,  
+                           save_txt=True,   
+                           save_conf=True,  
+                           save_hybrid=True,  
+                           plots=True)  
+        
+        metrics = {
+            'experiment_name': Path(model_path).parent.parent.name,
+            'model_parameters': model.info()[1],
+            'GFLOPs': model.info()[3],
+            'precision': result.box.mp,
+            'recall': result.box.mr,
+            'F1' : result.box.f1[0],
+            'map50': result.box.map50,
+            'map50_95': result.box.map,
+            'fitness': result.fitness,
+            'preprocessing_time': result.speed['preprocess'],
+            'inference_time': result.speed['inference'],
+            'postprocessing_time': result.speed['postprocess'],
+            'time_loss': result.speed['loss'],
+            'total_time': result.speed['inference'] + result.speed['postprocess'] + result.speed['loss'] + result.speed['preprocess'],
         }
+        results.append(metrics)
+    # Truncate values to 4 decimal places
+    for result in results:
+        for key, value in result.items():
+            if isinstance(value, float):
+                result[key] = round(value, 4)
+    pd.DataFrame(results).to_csv('evaluation_results.csv', index=True)
+    print(pd.DataFrame(results))
 
-result_df = pd.DataFrame([metrics])
+if __name__ == "__main__":
+    evaluate(all_models)
 
-# Export results to CSV
-output_csv = f'{metrics["experiment_name"]}_test.csv'
-result_df.to_csv(output_csv, index=False)
 
-print(result_df)
+# best_model_dir = "../experiments/Lila.v1.3k_y10s_ep100/weights/best.pt"
+# model = ultics.YOLO(best_model_dir)
+# data_config = "../data/Lila.v1.3k/data.yaml"
+
+# results = model.val(data=data_config,
+#                     split='val',
+#                     save_json=True,  
+#                     save_txt=True,   
+#                     save_conf=True,  
+#                     save_hybrid=True,  
+#                     plots=True)  
+
+# metrics = {
+#             'model_name': Path(best_model_dir).name,
+#             'model_path': best_model_dir,
+#             'experiment_name': Path(best_model_dir).parent.parent.name,
+#             'precision': results.box.mp ,
+#             'recall': results.box.mr,
+#             'F1': results.box.f1,
+#             'map50': results.box.map50,
+#             'map50_95': results.box.map,
+#             'fitness': results.fitness,
+#             'preprocessing_time': results.speed['preprocess'],
+#             'inference_time': results.speed['inference'],
+#             'postprocessing_time': results.speed['postprocess'],
+#             'time_loss': results.speed['loss'],
+#             'total_time': results.speed['inference'] + results.speed['postprocess'] + results.speed['loss'] + results.speed['preprocess'],
+#         }
+
+# result_df = pd.DataFrame([metrics])
+
+# # Export results to CSV
+# output_csv = f'{metrics["experiment_name"]}_test.csv'
+# result_df.to_csv(output_csv, index=False)
+
+# print(result_df)
+
+'''
+with open('../runs/detect/val12/predictions.json', 'r') as f:
+    results = yaml.safe_load(f)
+
+img_id = 'loc_0090_im_000046'
+pred_boxes = []
+
+for result in results:
+    if result['image_id'] == img_id and result['score'] > 0.5:
+        pred_boxes.append(result['bbox'])
+
+print(pred_boxes)
+
+true_label_dir = '../data/Lila.v1.3k/valid/labels'
+with open(f'{true_label_dir}/{img_id}.txt', 'r') as f:
+    true_boxes = [line.strip().split()[1:] for line in f.readlines()]
+
+img = cv2.imread(f'../data/Lila.v1.3k/valid/images/{img_id}.jpg')
+
+for box in pred_boxes:
+    # Convert YOLO prediction format to bounding box coordinates
+    x_corner, y_corner, width, height = map(float, box)
+    x1 = int((x_corner ) )
+    y1 = int((y_corner ))
+    x2 = int((x_corner + width ) )
+    y2 = int((y_corner + height) )
+    cv2.rectangle(img, (x1, y1), (x2, y2), (0, 0, 255), 4)
+    
+print(true_boxes)
+    
+for box_points in true_boxes:
+    x_center, y_center, width, height = map(float, box_points)
+    x1 = int((x_center - width / 2) * img.shape[1])
+    y1 = int((y_center - height / 2) * img.shape[0])
+    x2 = int((x_center + width / 2) * img.shape[1])
+    y2 = int((y_center + height / 2) * img.shape[0])
+    cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 4)
+
+plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+plt.savefig('true_v_pred_bbox.png')
+'''
