8f5711b47e1e64fc197eb1fe102dfc1338003ec9
WinnerBishal <winnerbishal9848@gmail.com>
2025-08-08 08:48:50 -0500

    new module to maintain all .pt paths

---
+++ notebooks/eval2export/model_paths.py
@@ -0,0 +1,23 @@
+# model_paths.py
+# Centralized model path definitions for all scripts
+
+yolo8n = "experiments/Lila.v1.3k_y8n_ep100/weights/best.pt"
+yolo8s = "experiments/Lila.v1.3k_y8s_ep100/weights/best.pt"
+yolo8m = "experiments/Lila.v1.3k_y8m_ep100/weights/best.pt"
+
+yolo9t = "experiments/Lila.v1.3k_y9t_ep100/weights/best.pt"
+yolo9s = "experiments/Lila.v1.3k_y9s_ep100/weights/best.pt"
+yolo9m = "experiments/Lila.v1.3k_y9m_ep100/weights/best.pt"
+
+yolo10n = "experiments/Lila.v1.3k_y10n_ep100/weights/best.pt"
+yolo10s = "experiments/Lila.v1.3k_y10s_ep100/weights/best.pt"
+yolo10m = "experiments/Lila.v1.3k_y10m_ep100/weights/best.pt"
+
+yolo11n = "experiments/Lila.v1.3k_y11n_ep100/weights/best.pt"
+yolo11s = "experiments/Lila.v1.3k_y11s_ep100/weights/best.pt"
+yolo11m = "experiments/Lila.v1.3k_y11m_ep100/weights/best.pt"
+
+all_models = [yolo8n, yolo8s, yolo8m,
+              yolo9t, yolo9s, yolo9m,
+              yolo10n, yolo10s, yolo10m,
+              yolo11n, yolo11s, yolo11m]
